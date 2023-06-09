f686f840ee54fe5a92778db4f1730f1094ff16fa
kevincamussi <111162369+kevincamussi@users.noreply.github.com>
2023-06-08 21:04:31 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.getElementById('portfolio').onload=exibirImagem();
+
+/*****      Exibe as imagens no site   - Display images in website    *****/
+
+function exibirImagem(){
+  for(i=1; i<89 ; i++){
+    var img = document.createElement('img');
+    img.classList.add('img-portfolio');
+    img.src=`img/portfolio/img${i}.jpg`;
+    document.getElementById('enviaImg').appendChild(img);
+  }
+
+  /*****    Abrir imagem em outra tab  - Open images in new tab  ****/
+  
+  var enderecoImg = document.querySelectorAll('.img-portfolio');
+   enderecoImg.forEach(enderecoImg => {
+    enderecoImg.addEventListener('click',(e)=>{
+    window.open(e.target.src);
+    })
+ });
+}
+
+/**** Rolar ao topo  -  Scroll to top   ****/
+
+const button = document.getElementById('scroll');
+button.addEventListener('click',()=>{
+window.scrollTo(top);
+})
