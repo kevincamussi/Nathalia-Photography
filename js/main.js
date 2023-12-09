352b4320ab955fa7a5d8a26603b07e559efe5f2c
kevincamussi <111162369+kevincamussi@users.noreply.github.com>
2023-12-09 15:00:25 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ document.getElementById('portfolio').onload=exibirImagem();
 /*****      Exibe as imagens no site   - Display images in website    *****/
 
 function exibirImagem(){
-  for(i=1; i<89 ; i++){
+
+  const numeroFotos = 89;
+  
+  for(i=1; i<numeroFotos ; i++){
     var img = document.createElement('img');
     img.classList.add('img-portfolio');
     img.src=`img/portfolio/img${i}.jpg`;
